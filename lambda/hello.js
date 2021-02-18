d8d87f416875ace6d32127c124c198fed130b968
jeromedecoster <jeromedecoster@users.noreply.github.com>
2021-02-18 07:09:36 +0100

    Update hello.js

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from feature-1 v2'),
+        body: JSON.stringify('Hello from Lambda'),
     }
     return response
 }
