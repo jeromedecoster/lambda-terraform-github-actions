8ff90d92fc98be12f05155a0de7fbcd7105c3389
geirfor <147711041+geirfor@users.noreply.github.com>
2023-11-15 15:37:45 +0100

    Update hello.js

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda from me'),
+        body: JSON.stringify('Hello from develop'),
     }
     return response 
 }
